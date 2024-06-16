e62a41f0b94f98572edc91520ce2af6d8e82a78c
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-06-16 14:52:14 -0600

    feat (video.proto): including grpc video client

---
+++ src/CycleCare/GrpcClient/GrpcVideoClient.cs
@@ -0,0 +1,53 @@
+﻿using Grpc.Net.Client;
+using System;
+using Google.Protobuf;
+using System.Threading.Tasks;
+using System.IO;
+using static CycleCare.Protos.VideoService;
+using CycleCare.Protos;
+using Grpc.Core;
+using CycleCare.Utilities;
+
+namespace CycleCare.GrpcClients
+{
+    internal class GrpcVideoClient
+    {
+        private VideoServiceClient _client;
+        private GrpcChannel _channel = GrpcChannel.ForAddress(Properties.Resources.GRPC_URL);
+
+        public GrpcVideoClient()
+        {
+            _client = new VideoServiceClient(_channel);
+        }
+
+        public async Task DownloadVideo(string fileName)
+        {
+            try
+            {
+                using (var call = _client.streamVideo(new StreamVideoRequest
+                {
+                    Filename = fileName
+                }))
+                {
+                    using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        await foreach (var chunk in call.ResponseStream.ReadAllAsync())
+                        {
+                            fileStream.Write(chunk.Data.ToByteArray(), 0, chunk.Data.Length);
+                        }
+                    }
+                }
+            }
+            catch (RpcException e)
+            {
+                DialogManager.ShowErrorMessageBox("Error al descargar video");
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                await _channel.ShutdownAsync();
+            }
+        }
+
+    }
+}

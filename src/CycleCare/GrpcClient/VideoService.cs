caa877d56925edcb8317a383f9ad06ecba67f119
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-06-16 12:37:37 -0600

    chore: Update XML configuration and package versions

---
+++ src/CycleCare/GrpcClient/VideoService.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using System;
+using Google.Protobuf;
+using System.Threading.Tasks;
+using Grpc.Core;
+using System.IO;
+
+namespace CycleCare.GrpcClient
+{
+    public class VideoService
+    {
+        //private CycleService Client;
+        //private GrpcChannel Channel = GrpcChannel.ForAddress(Properties.Resources.GRPC_URL);
+
+        public VideoService()
+        {
+            //Client = new CycleService(Channel);
+        }
+    }
+}
